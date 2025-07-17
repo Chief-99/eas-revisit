8e5e9f45156dd4d901e8a2d1942349b7a227ab4e
Similo <171865838+Chief-99@users.noreply.github.com>
2025-07-17 15:09:54 +0200

    Create columns in each row

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
 const gridContainer = document.querySelector('#grid-container');
 
 function createGrid() {
-    for (let i = 0; i <= 16; i++) {
+    for (let i = 0; i < 16; i++) {
         let row = document.createElement('div');
         row.classList.add('row')
         
-        
-        
+        for (let j = 0; j < 16; j++) {
+            let column = document.createElement('div');
+            column.classList.add('column');
+            row.append(column);
+        }
         gridContainer.append(row);
+
     }
 }
 
