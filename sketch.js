049ed12f1230535b50ff8a065d4adc8815460779
Similo <171865838+Chief-99@users.noreply.github.com>
2025-07-17 18:20:43 +0200

    Declare grid size button in js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('#grid-container');
+const gridSizeButton = document.querySelector('#grid-size-button');
 
 function createGrid() {
     for (let i = 0; i < 16; i++) {
