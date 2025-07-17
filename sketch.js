5a85e060bcb467432e822c0e1a3dc0fbb5c494fc
Similo <171865838+Chief-99@users.noreply.github.com>
2025-07-17 19:39:16 +0200

    Create grid successfully with user input

--- sketch.js
+++ sketch.js
@@ -1,31 +1,37 @@
 const gridContainer = document.querySelector('#grid-container');
-const grid = document.querySelector('#grid');
+let grid = document.querySelector('#grid');
 const gridSizeButton = document.querySelector
 ('#grid-size-button');
 let squaresPerSide = 16;
 
-function createGrid() {
-    for (let i = 0; i < squaresPerSide; i++) {
+function createGrid(squares) {
+    if (grid.hasChildNodes()) {
+        grid.remove();
+        grid = document.createElement('div');
+        grid.id = 'grid';
+    }
+    for (let i = 0; i < squares; i++) {
         let row = document.createElement('div');
         row.classList.add('row')
         
-        for (let j = 0; j < squaresPerSide; j++) {
+        for (let j = 0; j < squares; j++) {
             let column = document.createElement('div');
             column.classList.add('cell');
             row.append(column);
         }
-        grid.append(row);
-        
+        grid.append(row);  
     }
+
+    gridContainer.appendChild(grid);
     
 }
 
 
-createGrid();
+createGrid(squaresPerSide);
 
 gridSizeButton.addEventListener('click', function() {
     squaresPerSide = Number(prompt('Enter number of squares per side:', ''));
-    createGrid();
+    createGrid(squaresPerSide);
 })
 
 const cells = document.querySelectorAll('.cell');
