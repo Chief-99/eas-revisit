be7906c2a6369d479d43706cb83a768ab79ad29e
Similo <171865838+Chief-99@users.noreply.github.com>
2025-07-17 14:41:56 +0200

    Give rows and columns classes after creation

--- sketch.js
+++ sketch.js
@@ -3,12 +3,16 @@ const gridContainer = document.querySelector('#grid-container');
 function createGrid() {
     for (let i = 0; i <= 16; i++) {
         let row = document.createElement('div');
+        row.classList.add('row')
         
         for (let j = 0; j <= 16; j++) {
             let column = document.createElement('div');
-            row.append(column);
+            column.classList.add('column');
+            row.appendChild(column);
         }
         
-        gridContainer.append(row);
+        gridContainer.appendChild(row);
     }
 }
+
+createGrid();
