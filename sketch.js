2842b87aa97c7807934444d71cc0ad82f6a3af7f
Similo <171865838+Chief-99@users.noreply.github.com>
2025-07-27 22:30:58 +0200

    Style cells to appear on grid

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const grid = document.getElementById('grid');
 function createGrid() {
     for (let i = 0; i < 16; i++) {
         let row = document.createElement('div');
+        row.classList.add('row');
         for (let j = 0; j < 16; j++) {
             let column = document.createElement('div');
             column.classList.add('cell');
