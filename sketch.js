dfa0d9d4b81499909acd61a66d78f40b15456467
Similo <171865838+Chief-99@users.noreply.github.com>
2025-07-27 22:20:02 +0200

    Write function to create a grid

--- sketch.js
+++ sketch.js
@@ -2,5 +2,15 @@ const gridContainer = document.getElementById('grid-container');
 const grid = document.getElementById('grid');
 
 function createGrid() {
-    
+    for (let i = 0; i < 16; i++) {
+        let row = document.createElement('div');
+        for (let j = 0; j < 16; j++) {
+            let column = document.createElement('div');
+            column.classList.add('cell');
+            row.append(column);
+        }
+        grid.append(row);
+    }
 }
+
+createGrid();
